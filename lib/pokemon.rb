96b389fccf43fba1127ebbc497738409c9478e3b
James Scalise <jamescscalise@gmail.com>
2019-10-29 22:48:11 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
 class Pokemon
+  attr_accessor :name, :type, :id, :db
+  def initialize(name:, type:, id:, db:)
+    @name = name
+    @type = type
+    @id = id
+    @db = db
+  end
+  
+ def self.save(name, type, db)
+    
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) 
+      VALUES (?, ?)
+    SQL
+ 
+    db.execute(sql, name, type)
+ 
+  end
+  
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT * FROM pokemon
+      WHERE id = ?
+    SQL
+    
+    row = db.execute(sql, id)[0]
+    
+    pokemon = Pokemon.new(row[1],row[2], id, db)
+  end
 end
