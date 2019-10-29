941253a0ead1daf9a6373e2d1da87ec2f345c932
James Scalise <jamescscalise@gmail.com>
2019-10-29 22:51:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -26,7 +26,7 @@ class Pokemon
     
     row = db.execute(sql, id)[0]
     
-    binding.pry
+   # binding.pry
     
     pokemon = Pokemon.new(name: row[1],type: row[2], id: id, db: db)
     pokemon
